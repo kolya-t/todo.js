2a5a3801d1b719ae474556dd00e2fbd0c50ac31e
Sturdystablestalwart <72163215+sturdystablestalwart@users.noreply.github.com>
2020-10-25 15:14:27 +0300

    Done separation on functions and dynamic text change in page

--- 1st_script.js
+++ 1st_script.js
@@ -1,31 +1,46 @@
-$(document).ready(function(){
+function Change_is_done(item, aaa){
+    aaa.on('click',function(event){
+
+        var opposit_of_is_done = 1 - $('a#' + item.id).attr(("is_done"))
+        var patch = {'is_done': opposit_of_is_done}
+        //window.console&&console.log(patch)
+        $.ajax({
+            type: 'PATCH',
+            url: 'http://localhost:5000/tasks/' + item.id,
+            data: JSON.stringify(patch),
+            processData: false,
+            contentType: 'application/json',
+            success: function(){
+                $('a#' + item.id).attr("is_done", opposit_of_is_done)
+            },
+            error: function(){
+                alert('An error accured!');
+            }
+        });
+    })
+};
+
+function Initial_list_load() {
     $.getJSON('http://localhost:5000/tasks', function(data) {
-    	//window.console&&console.log(data)
-    	var TheList = $('ul.dzamilpersaneg');
+        window.console&&console.log(data)
+        var TheList = $('ul.dzamilpersaneg');
 
-    	$.each(data, function(i,item){
-			var aaa = $('<a/>')
-				.text(item.description + ' ' + item.is_done);
+        $.each(data, function(i,item){
+            var aaa = $('<a/>')
+                .attr("id", item.id)
+                .attr("is_done", item.is_done)
+                .text(item.description);
 
-			aaa.on('click', function (event) { 
-				console.log('clicked' + item.id)
 
-				var patch = {'is_done': 1 - item.is_done}
-				console.log(patch)
-				$.ajax({
-				   type: 'PATCH',
-				   url: 'http://localhost:5000/tasks/' + item.id,
-				   data: JSON.stringify(patch),
-				   processData: false,
-				   contentType: 'application/json',
-				});
-			})
+            Change_is_done(item,aaa)
 
-	    	var li = $('<li/>')
-				.addClass('Quask')
-				.append(aaa);
+            var li = $('<li/>')
+                .addClass('Quask')
+                .append(aaa);
 
-	    	TheList.append(li);
-    	});
+            TheList.append(li);            
+        });
     });
-});
+};
+
+$(document).ready(Initial_list_load())
