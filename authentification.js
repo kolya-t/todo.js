436b03464803e7b55617222dea9d8bd54ca2dc07
ntop <ntop@example.com>
2020-11-01 00:26:55 +0300

    fixed

--- authentification.js
+++ authentification.js
@@ -1,6 +1,7 @@
 function first_autentification(){    
     $('form.cookieform').show()
-    $('input.page').click(function(){
+    $('input.page').click(function(e){
+        e.preventDefault();
         login_pare = $('input[name="username"]').val() + ':' + $('input[name="password"]').val()
         Initial_list_load(login_pare)
     })
