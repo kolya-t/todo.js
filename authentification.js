a38184fb0259d974999e0138bddcbbe5ff836db2
sturdystablestalwart <laosen@mail.ru>
2020-11-09 12:07:38 +0300

    Added localstorage check before form.show()

--- authentification.js
+++ authentification.js
@@ -1,10 +1,15 @@
-function first_autentification(){    
-    $('form.cookieform').show()
-    $('input.page').click(function(e){
-        e.preventDefault();
-        localStorage.setItem('login_pare', btoa($('input[name="username"]').val() + ':' + $('input[name="password"]').val()))
-        Initial_list_load(localStorage.getItem('login_pare'))
-    })
+function first_autentification(){
+	if (localStorage.getItem('login_pare') != '') {
+    	Initial_list_load(localStorage.getItem('login_pare'))
+    }
+    else{    
+	    $('form.cookieform').show()
+	    $('input.page').click(function(e){
+	        e.preventDefault();
+	        localStorage.setItem('login_pare', btoa($('input[name="username"]').val() + ':' + $('input[name="password"]').val()))
+	    	Initial_list_load(localStorage.getItem('login_pare'))
+    	})
+    }
 }
 
 $(document).ready(first_autentification)
